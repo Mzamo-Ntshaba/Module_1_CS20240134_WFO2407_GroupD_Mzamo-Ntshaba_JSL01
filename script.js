84a3b61be3535f464433c08195f4e979fb9ca01d
Mzamo Ntshaba <mzamontshaba14@gmail.com>
2024-08-30 16:49:08 +0200

    Javascript code to validate syntax

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 function validateSyntax() {
+ 
     let input = document.getElementById('petInput').value;
     // Validation logic goes here
-    let result = ''; // Placeholder for validation result
+    // declare result here
+    let result= "";
+    const validPattern =/^pet_\d{4}[^\d_]+$/;
 
-    // TODO: Write your validation logic here
-        // Check if input starts with 'pet_' and followed by alphanumeric characters
+    if (validPattern.test(input)){
+        //valid syntax
+        result = `Valid Syntax \u{1F4AF}`;
+        alert("Well Done !!!");
+        
+    }  else{
+        //invalid syntax
+        result = `Invalid Syntax \u{1FAE3} `;
+        alert("Try Again and Enter Valid Syntax");
+    }
 
-            document.getElementById('result').innerText = result;
+    // output on the screen using id 'result'
+    document.getElementById('result').textContent = result;
 }
-
 
